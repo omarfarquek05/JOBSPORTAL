cdf1e6e175eae75e2930d83541341febdf432441
omar farquek joy <farquekomar@gmail.com>
2023-11-08 21:04:00 +0600

    first commit

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,18 +1,38 @@
 import mongoose from "mongoose";
 
-export function connectDB() {
-
+export async function connectDB() {
     try {
-        mongoose.connect(process.env.MONGODB_URL);
-       
-        const connection = mongoose.connection;
-          connection.on("connected" , ()=> {
-            console.log("MongoDB Connection Successfull");
-          });
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
 
+        const connection = mongoose.connection;
+        connection.on("connected", () => {
+            console.log("MongoDB Connection Successful");
+        });
     } catch (error) {
-        console.log(error);     
+        console.error("MongoDB Connection Error:", error);
     }
 }
+
+
+
+// import mongoose from "mongoose";
+
+// export function connectDB() {
+
+//     try {
+//         mongoose.connect(process.env.MONGODB_URL);
+       
+//         const connection = mongoose.connection;
+//           connection.on("connected" , ()=> {
+//             console.log("MongoDB Connection Successfull");
+//           });
+
+//     } catch (error) {
+//         console.log(error);     
+//     }
+// }
 
 
